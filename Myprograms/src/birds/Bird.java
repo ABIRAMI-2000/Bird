10ffc8e8378f088d7cfc299dd5c2845b329ac31d
ABIRAMI-2000 <84631335+ABIRAMI-2000@users.noreply.github.com>
2021-06-17 14:59:46 +0530

    First commit uploading all files

---
+++ Myprograms/src/birds/Bird.java
@@ -0,0 +1,14 @@
+package birds;
+
+public class Bird {
+	public void fly() {
+		System.out.println(" Am flying");
+	}
+
+	public static void main(String[] args) {
+		Bird obj = new Bird();
+		obj.fly();
+
+	}
+
+}
